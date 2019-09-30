2ab45a27f8d12bacceb0acff7baa003e143d4622
hamedhajiloo <hamedhajiloo96@gmail.com>
2019-09-30 17:49:46 +0330

    Edit

--- Web/Areas/Admin/Controllers/HomeController.cs
+++ Web/Areas/Admin/Controllers/HomeController.cs
@@ -22,12 +22,14 @@ namespace newsSite90tv.Areas.Admin.Controllers
     {
         private readonly IRepository<Book> _bRepository;
         private readonly IRepository<User> _uRepository;
+        private readonly IRepository<ReserveBook> _rbRepository;
         private readonly UserManager<User> _userManager;
 
-        public HomeController(IRepository<Book> bRepository, IRepository<User> uRepository, UserManager<User> userManager)
+        public HomeController(IRepository<Book> bRepository, IRepository<User> uRepository, IRepository<ReserveBook> rbRepository, UserManager<User> userManager)
         {
             this._bRepository = bRepository ?? throw new ArgumentNullException(nameof(bRepository));
             this._uRepository = uRepository ?? throw new ArgumentNullException(nameof(uRepository));
+            this._rbRepository = rbRepository ?? throw new ArgumentNullException(nameof(rbRepository));
             this._userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
         [Route("")]
@@ -38,15 +40,20 @@ namespace newsSite90tv.Areas.Admin.Controllers
             var teachers= await _userManager.GetUsersInRoleAsync("Teacher");
             var model = new IndexModel
             {
-                AllBook = await _bRepository.TableNoTracking.Where(c => c.BookIsDeleted == false).CountAsync(cancellationToken),
+                BorrowedBook = await _rbRepository.TableNoTracking
+                .Where(c => c.BookStatus == BookStatus.Borrowed).CountAsync(cancellationToken),
 
-                BorrowedBook = await _bRepository.TableNoTracking.Where(c => c.BookIsDeleted == false /*&& c.BookStatus == BookStatus.Borrowed*/).CountAsync(cancellationToken),
+                //FreeBook = await _bRepository.TableNoTracking.Where(c => c.BookIsDeleted == false /*&& c.BookStatus == BookStatus.Free*/).CountAsync(cancellationToken),
 
-                FreeBook = await _bRepository.TableNoTracking.Where(c => c.BookIsDeleted == false /*&& c.BookStatus == BookStatus.Free*/).CountAsync(cancellationToken),
-
-                ReserveBook = await _bRepository.TableNoTracking.Where(c => c.BookIsDeleted == false/* && c.BookStatus == BookStatus.Reserved*/).CountAsync(cancellationToken),
-
+                ReserveBook = await _rbRepository.TableNoTracking
+                .Where(c => c.BookStatus == BookStatus.Reserved).CountAsync(cancellationToken)
             };
+            var books = await _bRepository.TableNoTracking.Include(c=>c.ISBNs).Where(c => c.BookIsDeleted == false).ToListAsync(cancellationToken);
+            foreach (var book in books)
+            {
+                model.AllBook += book.ISBNs.Where(c => c.IsDeleted == false).Count();
+            }
+
             //personel
             if (personels!=null&&personels.Count>0)
             {
